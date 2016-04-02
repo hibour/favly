9ddb7c99071a12acdc1d5eded17856b18bba7707
Nageswara Rao Mannem <nageshwara@gmail.com>
2016-04-02 16:19:19 -0700

    small styling change

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -2,7 +2,6 @@
 var React = require('react-native');
 var {
   StyleSheet,
-  Text,
   View,
   Picker,
   Component,
@@ -12,19 +11,27 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import SettingsActions from '../actions/settings'
 
+import Title from './text/Title'
+
 class Settings extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return (<View style={this.props.style}>
-      <Picker
-        selectedValue={this.props.locale}
-        onValueChange={(locale) => this.props.changeLocale(locale)}>
-        <Picker.Item label="Telugu" value="te" />
-        <Picker.Item label="English" value="en" />
-      </Picker>
+    return (
+      <View style={this.props.style}>
+        <View style={styles.row}>
+          <Title>Preferred Language</Title>
+          <Picker style={styles.control}
+            selectedValue={this.props.locale}
+            mode='dropdown'
+            onValueChange={(locale) => this.props.changeLocale(locale)}>
+            <Picker.Item label="Telugu" value="te" />
+            <Picker.Item label="English" value="en" />
+          </Picker>
+        </View>
+      <View style={styles.separator} />
     </View>);
   }
 }
@@ -40,4 +47,19 @@ function mapDispatchToProps(dispatch) {
 module.exports = connect(mapStateToProps, mapDispatchToProps)(Settings)
 
 const styles = StyleSheet.create({
+  row: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 4,
+    flex: 1,
+    flexDirection: 'column',
+  },
+  separator: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#bbbbbb',
+  },
+  control: {
+    height: 10,
+  }
 });
